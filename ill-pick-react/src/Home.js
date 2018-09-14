748a574e5ebf0631cc482ee99c90e016228676b4
DeusMalsith <justinrashallphotography@gmail.com>
2018-09-13 17:46:41 -0700

    Fixed button and did some slight tweaks

--- ill-pick-react/src/Home.js
+++ ill-pick-react/src/Home.js
@@ -10,11 +10,10 @@ class Home extends Component {
         <div className="home row">
         	<div className="col s12 center">
         		<h3>Press the "Try it! button to get a random dish!</h3>
-        		<br />
         		<h1>SAMPLE TEXT</h1>
         		<br />
 
-		        <button class="btn waves-effect waves-light amber" type="submit">Try it!</button>        		
+		        <button class="btn waves-effect waves-light amber tryit" type="submit">Try it!</button>        		
         	</div>
         </div>        
       </div>
