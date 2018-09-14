3b8e92a414165096baa09ee8dba2be0f6cff23f8
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-14 01:21:46 -0700

    create profile try it button

--- ill-pick-react/src/Home.js
+++ ill-pick-react/src/Home.js
@@ -20,7 +20,7 @@ class Home extends Component {
   constructor(props){
     super(props);
     this.state = {
-      name: 'TEST '
+      name: ''
     }
   }
 
--- ill-pick-react/src/Profile.js
+++ ill-pick-react/src/Profile.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import SERVER_URL from './constants/server';
 
 class Profile extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: ''
+    }
+  }
+
+  handleFavorite = (e) => {
+    e.preventDefault();
+    console.log('step 1', this.state);
+    axios.get(SERVER_URL + '/users/RandFave')
+      .then( result => {
+        console.log('SUCCESS', result)
+        this.setState({
+          name: result.data.name
+        })
+      }).catch( err => {
+        console.log('THERE WAS AN ERROR!')
+      })
+    console.log('The name is: ', this.state.name)
+  }
 
 
   render() {
     if(this.props.user){
       return (
-          <div>
-            <button className="random-search" type="button">Try It!</button>
-          </div>
+      <div className="container">
+        <div className="home row">
+        	<div className="col s12 center">
+        		<h3>Press the "Try it! button to get a favorite dish!</h3>
+        		<h1 className="">{this.state.name}</h1>
+        		<br />
+
+		        <button class="btn waves-effect waves-light amber tryit" type="submit" onClick={this.handleFavorite}>Get a fav!</button>        		
+        	</div>
+        </div>        
+      </div>
         );
     }
     return(
