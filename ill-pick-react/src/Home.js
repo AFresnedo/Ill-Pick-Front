3f4cce6e6568d22bc2e31eecfb7b04ea1d06529f
DeusMalsith <justinrashallphotography@gmail.com>
2018-09-13 17:39:21 -0700

    Added home page temp design

--- ill-pick-react/src/Home.js
+++ ill-pick-react/src/Home.js
@@ -6,10 +6,17 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div className="">
-        
-        <button className="random-search" type="button">Try It!</button>
-        
+      <div className="container">
+        <div className="home row">
+        	<div className="col s12 center">
+        		<h3>Press the "Try it! button to get a random dish!</h3>
+        		<br />
+        		<h1>SAMPLE TEXT</h1>
+        		<br />
+
+		        <button class="btn waves-effect waves-light amber" type="submit">Try it!</button>        		
+        	</div>
+        </div>        
       </div>
     );
   }
